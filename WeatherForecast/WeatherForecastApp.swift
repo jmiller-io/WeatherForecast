97c83ab3921be7ce6b3115ff7ea45e79e07f6b68
Jake Miller <hillsighed@gmail.com>
2024-10-24 15:12:37 -0500

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Jake Miller on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
